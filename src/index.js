fa9ac964ef4da9ffe3f6c0f926a140fb3df4a88e
GoitStudentsWorks <129747846+GoitStudentsWorks@users.noreply.github.com>
2023-10-13 14:16:21 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/goose-track-project">
+        <BrowserRouter basename="fs-75-2-react-node-GooseTrack">
           <App />
         </BrowserRouter>
       </PersistGate>
